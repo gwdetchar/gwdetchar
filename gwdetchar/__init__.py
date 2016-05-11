38953a49716832ea6f6b10532a72169822caf693
Duncan Macleod <duncan.macleod@ligo.org>
2016-05-11 16:46:44 -0500

    use versioneer for git versioning

--- gwdetchar/__init__.py
+++ gwdetchar/__init__.py
@@ -27,3 +27,7 @@ from . import version
 
 __version__ = version.version
 __author__ = 'Duncan Macleod <duncan.macleod@ligo.org>'
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
