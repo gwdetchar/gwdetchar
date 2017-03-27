6f57890f53806054b9585e153ea446ac38054544
uberpye <uber_pye@csu.fullerton.edu>
2017-03-27 12:00:44 -0700

    updating (#12)
    * scattering: set any unit to velocity timeseries
    
    prevents errors with astropy 1.3, can probably be removed when `TimeSeries.diff` properly protects metadata
    
    * Added recursive outlier removal and static filter settings
    
    * Fixed print statement
    
    * Fixed print statement
    
    * gwdetchar-slow-correlation: catch RuntimeErrors from latex and retry
    
    * gwdetchar-overflow: added --fec-map option
    
    just adds a link to the bottom of the HTML
    
    * Changed file name and directory writing functions
    
    * Tells when code is done running (#52)
    
    * Tells when code is done running
    
    Added a `--Process Completed` message to print when the script is done running all the way through so we can tell whether or not it is still running.
    
    * Update gwdetchar-slow-correlation
    
    * gwdetchar-overflows: fixed bug in overflows
    
    fixes #53
    
    * travis-ci: fixed build matrix
    
    re-formatted .travis.yml to get allow_failures back
    
    * gwdetchar-software-saturations: simplify TimeSeriesDict.read calls
    
    so they work with the new gwpy lalframe API
    
    * use `int` to get seconds from LIGOTimeGPS
    
    * gwdetchar-scattering: fixed typo
    
    * gwdetchar-scattering: fixed another typo
    
    * gwdetchar-scattering: force overwrite existing xml

--- gwdetchar/daq.py
+++ gwdetchar/daq.py
@@ -123,7 +123,7 @@ def ligo_model_overflow_channels(dcuid, ifo=None, frametype=None, gpstime=None,
     if frametype is None:
         frametype = '%s_R' % ifo
     if gpstime is None:
-        gpstime = tconvert().seconds - 1000
+        gpstime = int(tconvert()) - 1000
     try:
         framefile = find_frames(ifo[0], frametype, gpstime, gpstime)[0].path
     except IndexError as e:
