295150d15d0e7be95988f94d3175f4f8c564bfe3
Duncan Macleod <duncan.macleod@ligo.org>
2017-03-31 09:53:36 -0500

    setup.py: updated glue/dqsegdb requirements

--- setup.py
+++ setup.py
@@ -95,10 +95,10 @@ setup(name=DISTNAME,
       requires=requires,
       extras_require=extras_require,
       dependency_links=[
-          'http://software.ligo.org/lscsoft/source/glue-1.49.1.tar.gz'
-              '#egg=glue-1.49.1',
-          'http://software.ligo.org/lscsoft/source/dqsegdb-1.2.2.tar.gz'
-              '#egg=dqsegdb-1.2.2',
+          'http://software.ligo.org/lscsoft/source/glue-1.54.1.tar.gz'
+              '#egg=glue-1.54.1',
+          'http://software.ligo.org/lscsoft/source/dqsegdb-1.4.0.tar.gz'
+              '#egg=dqsegdb-1.4.0',
           'https://github.com/ligovirgo/trigfind/archive/v0.3.tar.gz'
               '#egg=trigfind-0.3',
       ],
