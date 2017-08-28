6aaa94a6404f6d3238fdf8b189d8e401f2f66841
Duncan Macleod <duncan.macleod@ligo.org>
2017-08-28 09:57:02 -0500

    io.datafind: added function to write cache in omega format

--- gwdetchar/io/datafind.py
+++ gwdetchar/io/datafind.py
@@ -19,6 +19,12 @@
 """gw_data_find wrappers
 """
 
+from __future__ import print_function
+
+import os.path
+
+from six import string_types
+
 from glue import datafind
 
 __author__ = 'Duncan Macleod <duncan.macleod@ligo.org>'
@@ -41,3 +47,36 @@ def find_frames(site, frametype, gpsstart, gpsend, **kwargs):
     kwargs.setdefault('urltype', 'file')
     return connection.find_frame_urls(site[0], frametype, gpsstart, gpsend,
                                       **kwargs)
+
+
+def write_omega_cache(cache, fobj):
+    """Write a :class:`~glue.lal.Cache` of files to file in Omega format
+
+    The Omega pipeline expects a file cache in a specific, custom format:
+
+    {observatory} {frametype} {gpsstart} {gpsend} {fileduration} {directory}
+    """
+    # open filepath
+    if isinstance(fobj, string_types):
+        with open(fobj, 'w') as f:
+            return write_omega_cache(cache, f)
+
+    # convert to omega cache format
+    wcache = {}
+    for e in cache:
+        dir_ = os.path.split(e.path)[0]
+        lfn = os.path.basename(e.path)
+        if dir_ in wcache:
+            l = wcache[dir_]
+            if l[2] > int(e.segment[0]):
+                wcache[dir_][2] = e.segment[0]
+            if l[3] < int(e.segment[1]):
+                wcache[dir_][3] = e.segment[1]
+        else:
+            wcache[dir_] = [e.observatory, e.description,
+                            int(e.segment[0]), int(e.segment[1]),
+                            int(abs(e.segment)), dir_]
+
+    # write to file
+    for item in wcache:
+        print(' '.join(map(str, wcache[item])), file=fobj)
