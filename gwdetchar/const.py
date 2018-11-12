21968a9a2893815c14606e40791b6ace4f8747c8
Duncan Macleod <duncan.macleod@ligo.org>
2018-11-12 06:30:36 -0600

    gwdetchar.const: store epochs in order
    and added O3 epoch

--- gwdetchar/const.py
+++ gwdetchar/const.py
@@ -20,6 +20,7 @@
 """
 
 import os
+from collections import OrderedDict
 
 from gwpy.segments import Segment
 
@@ -34,11 +35,14 @@ S6_SEGMENT_SERVER = os.getenv('S6_SEGMENT_SERVER',
                               'https://segdb.ligo.caltech.edu')
 
 # -- Run epochs ---------------------------------------------------------------
-EPOCH = {
-    'ER7': Segment(1117400416, 1118329216),
-    'ER8': Segment(1123858817, 1126623617),
-    'O1': Segment(1126623617, 1136649617),
-    'ER9': Segment(1152136817, 1152255617),
-    'ER10': Segment(1161907217, 1164499217),
-    'O2': Segment(1164499217, 2000000000),
-}
+
+EPOCH = OrderedDict([
+    ('S6', Segment(931035615, 971622015)),
+    ('ER7', Segment(1117400416, 1118329216)),
+    ('ER8', Segment(1123858817, 1126623617)),
+    ('O1', Segment(1126623617, 1136649617)),
+    ('ER9', Segment(1152136817, 1152255617)),
+    ('ER10', Segment(1161907217, 1164499217)),
+    ('O2', Segment(1164499217, 1187733618)),
+    ('O3', Segment(1187733618, 2000000000)),
+])
