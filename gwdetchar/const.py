46695ee8012348a619202ea7d3bc8202710bcf69
Duncan Macleod <duncan.macleod@ligo.org>
2018-11-14 16:46:02 +0000

    gwdetchar.const: use DEFAULT_SEGMENT_SERVER variable
    this is provided on LDG

--- gwdetchar/const.py
+++ gwdetchar/const.py
@@ -30,7 +30,9 @@ IFO = os.getenv('IFO', None)
 ifo = os.getenv('ifo', IFO.lower() if IFO else None)
 SITE = os.getenv('SITE', None)
 site = os.getenv('site', SITE.lower() if SITE else None)
-O1_SEGMENT_SERVER = os.getenv('O1_SEGMENT_SERVER', 'https://segments.ligo.org')
+
+DEFAULT_SEGMENT_SERVER = os.getenv('DEFAULT_SEGMENT_SERVER',
+                                   'https://segments.ligo.org')
 S6_SEGMENT_SERVER = os.getenv('S6_SEGMENT_SERVER',
                               'https://segdb.ligo.caltech.edu')
 
