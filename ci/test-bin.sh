d3e5ee7c9a56b1f996df6e06c07f090313a17e22
Duncan Macleod <duncanmmacleod@gmail.com>
2019-02-22 17:07:57 +0000

    Fixed testing of bin/ scripts in CI (#231)
    * ci: fixed testing of bin/ scripts
    
    * alphabetised dependencies
    
    easier to read
    
    * added missing pandas dependency
    
    * gwdetchar-slow-correlation: updated for gwpy-0.12.0
    
    * gwdetchar-lasso-correlation: fixed some deprecationwarnings
    
    * tweaked test script
    
    * ci: install with --editable
    
    should fix broken coverage appending
    
    * ci: print coverage report after testing

---
+++ ci/test-bin.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# Travis CI test runner: test --help for all command-line executables
+# Author: Duncan Macleod <duncan.macleod@ligo.org> (2019)
+
+FAILED=()
+
+echo "---- Testing scripts in /bin/ ----"
+
+# loop over all bin/ scripts
+for EXE in bin/*; do
+    # get file-name as PATH executable
+    EXENAME=$(basename ${EXE})
+    EXEPATH=$(which ${EXENAME})
+    if [ "$?" -ne 0 ]; then
+        FAILED+=(${EXENAME_})
+        continue
+    fi
+
+    # execute --help with coverage
+    echo "$ ${EXENAME} --help..."
+    python -m coverage run --append --source=gwdetchar ${EXEPATH} --help;
+    if [ "$?" -ne 0 ]; then
+        FAILED+=("${EXENAME}")
+        continue
+    fi
+done
+
+if [ ${#FAILED[@]} -ne 0 ]; then
+    echo "---- The following scripts failed: ----"
+    for failure in "${FAILED[@]}"; do
+        echo ${failure}
+    done
+    exit 1
+fi
+echo "---- All scripts passed ----"
