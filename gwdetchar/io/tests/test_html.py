f43779e1c824d9d826d1c9f8b1c429abe072edd4
Alexander Urban <alexander.urban@ligo.org>
2019-04-01 12:51:22 -0500

    Remove style declaration from unit tests

--- gwdetchar/io/tests/test_html.py
+++ gwdetchar/io/tests/test_html.py
@@ -77,7 +77,7 @@ HTML_FOOTER = """<footer class="footer">
 <div class="row">
 <div class="col-md-12">
 <p>This page was created by {user} at {date}.</p>
-<p><a href="https://github.com/gwdetchar/gwdetchar/tree/%s" target="_blank" style="color:#eee;">View gwdetchar-%s on GitHub</a> | <a href="https://github.com/gwdetchar/gwdetchar/issues" target="_blank" style="color:#eee;">Report an issue</a></p>
+<p><a href="https://github.com/gwdetchar/gwdetchar/tree/%s" target="_blank">View gwdetchar-%s on GitHub</a> | <a href="https://github.com/gwdetchar/gwdetchar/issues" target="_blank">Report an issue</a></p>
 </div>
 </div>
 </div>
