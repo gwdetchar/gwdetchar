49d200c632cc4869cf2c30bfd186fe6a15fe9be5
Alexander Urban <alexander.urban@ligo.org>
2019-04-04 15:59:18 -0500

    Resolve a build failure

--- gwdetchar/io/html.py
+++ gwdetchar/io/html.py
@@ -209,8 +209,8 @@ def new_bootstrap_page(base=os.path.curdir, lang='en', refresh=False,
         default: None
     """
     # get kwargs with sensible defaults
-    css = CSS_FILES.extend(kwargs.get('css', []))
-    script = JS_FILES.extend(kwargs.get('script', []))
+    css = CSS_FILES + kwargs.get('css', [])
+    script = JS_FILES + kwargs.get('script', [])
     # write CSS to static dir
     css, script = finalize_static_urls(
         os.path.join(os.path.curdir, 'static'),
