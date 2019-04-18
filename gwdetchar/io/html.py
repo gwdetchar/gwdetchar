1441992ce5eeb7ac66ad06563263e7685af58317
Alex L. Urban <aurban1@lsu.edu>
2019-04-18 09:10:00 -0500

    Pop CSS and JS files when constructing defaults (#314)

--- gwdetchar/io/html.py
+++ gwdetchar/io/html.py
@@ -282,8 +282,8 @@ def new_bootstrap_page(base=os.path.curdir, lang='en', refresh=False,
         default: None
     """
     # get kwargs with sensible defaults
-    css = kwargs.get('css', CSS_FILES)
-    script = kwargs.get('script', JS_FILES)
+    css = kwargs.pop('css', CSS_FILES)
+    script = kwargs.pop('script', JS_FILES)
     # write CSS to static dir
     css, script = finalize_static_urls(
         os.path.join(os.path.curdir, 'static'),
