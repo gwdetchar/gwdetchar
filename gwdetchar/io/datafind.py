68255e359a42f2a47847ae500a2653a562bdf923
Alex L. Urban <aurban1@lsu.edu>
2019-05-11 00:37:00 -0500

    Miscellaneous refinements on Lasso correlation (#320)
    * lasso: format results tables using in-built arguments to pandas DataFrame objects

--- gwdetchar/io/datafind.py
+++ gwdetchar/io/datafind.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with GW DetChar.  If not, see <http://www.gnu.org/licenses/>.
 
-"""gw_data_find wrappers
+"""Data discovery utilities
 """
 
 import re
