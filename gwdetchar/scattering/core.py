26ea6c923f57904f477396b631ca63effec5654d
siddharth soni <siddharthsoni22@gmail.com>
2019-06-05 15:52:09 -0500

    Update core.py

--- gwdetchar/scattering/core.py
+++ gwdetchar/scattering/core.py
@@ -121,7 +121,7 @@ def get_blrms(series, low=4.0, high=10.0, stride=1):
  
 def get_activesegs(series, name='scatsegs'):
     
-    series = [item for sublist in series for item in sublist] 
+    series = [item.value for sublist in series for item in sublist] 
     print(series)
     thresh = series > numpy.mean(series) + 6*numpy.std(series)
     threshflag = thresh.to_dqflag(name, round=True)
