6ff5ff78063a51ac8532b1f8f59d83e014eea0ea
Alex Urban <alexander.urban@ligo.org>
2019-10-24 01:28:44 -0500

    Use bootstrap.bundle.min.js

--- gwdetchar/io/tests/test_html.py
+++ gwdetchar/io/tests/test_html.py
@@ -62,7 +62,7 @@ NEW_BOOTSTRAP_PAGE = """<!DOCTYPE HTML>
 <link href="static/gwbootstrap.min.css" rel="stylesheet" media="all" />
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" type="text/javascript"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.lazy/1.7.9/jquery.lazy.min.js" type="text/javascript"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" type="text/javascript"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js" type="text/javascript"></script>
 <script src="static/gwbootstrap.min.js" type="text/javascript"></script>
 </head>
@@ -245,7 +245,7 @@ def test_finalize_static_urls(tmpdir):
         'https://cdnjs.cloudflare.com/ajax/libs/jquery.lazy/1.7.9/'
             'jquery.lazy.min.js',  # noqa: E131
         'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/'
-            'bootstrap.min.js',  # noqa: E131
+            'bootstrap.bundle.min.js',  # noqa: E131
         'https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/'
             'jquery.fancybox.min.js',  # noqa E131
         'static/gwbootstrap.min.js',
