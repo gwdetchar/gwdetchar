6a404da72f459858fdf26248b57b49665870dee8
Michael John Lowry <michaeljohn.lowry@ldas-grid.ligo.caltech.edu>
2021-10-01 13:42:05 -0700

    removed get_data() from test_main.py to avoid error when running CI in github

--- gwdetchar/lasso/tests/test_main.py
+++ gwdetchar/lasso/tests/test_main.py
@@ -1,14 +1,14 @@
 """Tests for gwdetchar.lasso.__main__
 """
 
-from .. import __main__ as lasso
-from ... import io
-
-from numpy import testing as nptest
-
 import tempfile
 
+import numpy as np
+
 from gwpy.timeseries import TimeSeries
+
+from .. import __main__ as lasso
+
 
 __author__ = 'Michael Lowry <michaeljohn.lowry@ligo.org>'
 
@@ -16,45 +16,29 @@ __author__ = 'Michael Lowry <michaeljohn.lowry@ligo.org>'
 # global test objects
 
 # volatile temp file to write expected timeseries to
-temp = tempfile.NamedTemporaryFile()
-test_channel = 'L1:DMT-SNSW_EFFECTIVE_RANGE_MPC.mean'
-test_frametype = 'SenseMonitor_CAL_L1_M'
-test_start = 1264983618
-test_end = test_start+7200
-expected_ts = io.datafind.get_data(test_channel, test_start,
-                                   test_end, frametype=test_frametype)
-expected_ts.write(temp, format='gwf')
+TEMP = tempfile.NamedTemporaryFile()
+TEST_CHANNEL = 'L1:DMT-SNSW_EFFECTIVE_RANGE_MPC.mean'
+TEST_START = 0
+TEST_END = TEST_START+1000
+TEST_ARRAY = np.linspace(TEST_START, TEST_END, num=TEST_END-TEST_START)
+expected_ts = TimeSeries(TEST_ARRAY, t0=TEST_START, dt=1, channel=TEST_CHANNEL)
+expected_ts.write(TEMP, format='gwf')
 
 
 # # -- unit tests -------------------------------------------------------------
 
 def test_read():
-    print('Testing primary channel file read...')
+    # test reading primary TimeSeries file
     # try finally for closing temp file
     try:
-        actual_ts = lasso.get_primary_ts(filepath=temp, channel=test_channel,
-                                         start=test_start, end=test_end,
-                                         frametype=test_frametype, cache=None,
-                                         nproc=1)
+        actual_ts = lasso.get_primary_ts(filepath=TEMP, channel=TEST_CHANNEL,
+                                         start=TEST_START, end=TEST_END,
+                                         cache=None, nproc=1)
     finally:
-        temp.close
+        TEMP.close
     assert actual_ts.t0 == expected_ts.t0
     assert actual_ts.times[-1] == expected_ts.times[-1]
     assert actual_ts.sample_rate == expected_ts.sample_rate
     assert actual_ts.dx == expected_ts.dx
-    nptest.assert_array_equal(actual_ts.value, expected_ts.value,
-                              err_msg='read in data array does not match')
-
-
-def test_get():
-    print('Testing primary channel query...')
-    actual_get_ts = lasso.get_primary_ts(channel=test_channel,
-                                         start=test_start, end=test_end,
-                                         frametype=test_frametype)
-    assert actual_get_ts.t0 == expected_ts.t0
-    assert actual_get_ts.times[-1] == expected_ts.times[-1]
-    assert actual_get_ts.sample_rate == expected_ts.sample_rate
-    assert actual_get_ts.dx == expected_ts.dx
-    nptest.assert_array_equal(actual_get_ts.value,
-                              expected_ts.value,
-                              err_msg='fetched data array does not match')
+    np.testing.assert_array_equal(actual_ts.value, expected_ts.value,
+                                  err_msg='read in data array does not match')
