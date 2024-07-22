8da529c4ca67b4801fef14117631ec7be090bd46
Evan Goetz <evan.goetz@ligo.org>
2024-07-22 09:41:48 -0700

    Remove lalsuite requirement

--- gwdetchar/io/ligolw.py
+++ gwdetchar/io/ligolw.py
@@ -27,7 +27,7 @@ try:
     from ligo.lw import lsctables
 except ModuleNotFoundError as exc:
     exc.msg = (
-        f"{exc.msg}, please install python-lal / python3-lal / lalsuite "
+        f"{exc.msg}, please install python-lal / lalsuite / python-ligo-lw "
         "to handle LIGO_LW files"
     )
     exc.args = (exc.msg,)
